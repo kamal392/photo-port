ceb5cb3b112d806abdc1e26bd19706180854ddc5
kamal kishore <kamal.kishore981@gmail.com>
2023-06-17 09:33:41 -0400

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-
+import React, { useState } from "react";
+import Nav from "./components/Nav";
+import About from "./components/About";
+import Gallery from "./components/Gallery";
+import ContactForm from "./components/Contact";
 
 function App() {
   // we lift up the categories state from a Nav component to App so that we can pass it to its children component.
@@ -20,23 +24,34 @@ function App() {
   // using useState hook to condition what user click on navigation bar.
   const [contactSelected, setContactSelected] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav
+        // here we have setup the attribute value so that we can pass it to Nav through props.
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+      ></Nav>
+      <main>
+        {/* <ContactForm></ContactForm>
+        <Gallery currentCategory={currentCategory}></Gallery>
+        <About></About> */}
+        {/* using condition to render contact component to render only when the use click on contact.
+        otherwise render <Gallery> and <About> */}
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+        ) : (
+          <ContactForm></ContactForm>
+        )}
+      </main>
     </div>
   );
 }
 
 export default App;
+
+
